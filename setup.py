94ea64cd68425826f6e81432a517c1e0086bc307
MrPickle311 <macmacroll@gmail.com>
2022-03-14 00:27:56 +0100

    Added enums and data models dirst

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['modbus', 'module_io', 'serial_io', 'module_utils', 'exposed_resources', 'http_io', 'i2c_io'],
+    packages=['modbus', 'module_io', 'serial_io', 'module_utils', 'exposed_resources', 'http_io', 'i2c_io', 'enums',
+              'data_models'],
     package_dir={'': 'scripts'})
 
 setup(**setup_args)
