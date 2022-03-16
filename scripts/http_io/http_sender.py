6014da5846f6c5f3bb59f15dc52d3074969faed5
MrPickle311 <macmacroll@gmail.com>
2022-03-16 12:35:47 +0100

    Implemented kss drone communication msgs. Fixed http_sender entities

--- scripts/http_io/http_sender.py
+++ scripts/http_io/http_sender.py
@@ -1,14 +1,14 @@
 import requests
 from pydantic import BaseModel
-
+from requests import Response
 
 class HttpSender:
 
-    def __init__(self, host_address: str, port: int, message_type: str, station_id: int):
+    def __init__(self, host_address: str, port: int, message_type: str, station_id: int, domain: str):
         self.ip_address = host_address
         self.port = port
         self.message_type = message_type
-        self.url_address = 'http://{0}:{1}/api/{2}'.format(self.ip_address, self.port, self.message_type)
+        self.url_address = 'http://{0}:{1}/{2}/{3}'.format(self.ip_address, self.port, domain, self.message_type)
         self.station_id = station_id
         self.station_id_str = 'station_id'
 
@@ -25,6 +25,7 @@ class HttpSender:
 
         self.additional_checking(response)
 
-    def send_message(self, data: BaseModel) -> None:
+    def send_message(self, data: BaseModel) -> Response:
         response = requests.post(url=self.url_address, json=data.dict())
         self.check_response_is_ok(response)
+        return response
