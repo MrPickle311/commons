cbb20da065fc8b1f250084f40d5282e6a7ee85ea
Damian Wójcik <macmacroll@gmail.com>
2022-04-17 12:58:58 +0200

    Added parameters service

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['modbus', 'module_io', 'serial_io', 'module_utils', 'exposed_resources', 'http_io', 'i2c_io', 'enums',
-              'data_models'],
+              'data_models', 'parameters'],
     package_dir={'': 'scripts'})
 
 setup(**setup_args)
